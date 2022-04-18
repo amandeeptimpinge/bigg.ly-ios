bc18ff5cc1e8cf954c92c58ee2a222f96d1063a2
Amandeep Tirhima <amandeeptirhima@admins-Mac-mini-new-simran.local>
2022-04-18 15:06:41 +0530

    latest code

---
+++ Bigg.ly/UploadViewController/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  Bigg.ly
+//
+//  Created by Navdeep Rana on 3/4/22.
+//
+
+import Foundation
---
+++ Bigg.ly/Uploader.swift
@@ -0,0 +1,8 @@
+//
+//  Uploader.swift
+//  Bigg.ly
+//
+//  Created by Navdeep Rana on 2/8/22.
+//
+
+import Foundation
